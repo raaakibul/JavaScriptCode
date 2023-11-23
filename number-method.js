793138ea27f19f223ee73adf00f9aaa913784cfe
rakibul-islam <raakibul.uap@gmail.com>
2023-11-23 19:39:54 +0600

    file added

--- number-method.js
+++ number-method.js
@@ -3,3 +3,10 @@ console.log(typeof(number));
 
 num = toString(number)
 console.log(typeof(num));
+// string concatenation
+var fullName = "Rakibul Islam"
+console.log("My name is " + fullName);
+
+var num1 = 20
+var num2 = 30
+console.log("Number 1 = "+ num1 + " Number 2 = "+ num2);
