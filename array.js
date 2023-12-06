2710db3c79009993fb2cc7f9c402633450d4c31a
rakibul-islam <raakibul.uap@gmail.com>
2023-12-06 23:42:14 +0600

    jani na

--- array.js
+++ array.js
@@ -1,14 +1,16 @@
-var names = new Array();
-names[0] ="Rakibul Islam"
-console.log(names[0]);
+// var names = new Array();
+// names[0] ="Rakibul Islam"
+// console.log(names[0]);
 
-names[1] = "Islam" 
+// names[1] = "Islam" 
 
-var number = new Array(20);
-const obj ={a:1, b:7, c:3, length:2};
-console.log(Object.keys(obj).length);
+// var number = new Array(20);
+// const obj ={a:1, b:7, c:3, length:2};
+// console.log(Object.keys(obj).length);
 
-const obj1= {module: 35, video:2}; const obj2= {module: 35, video:2};
-console.log(obj1 === obj2);
+// const obj1= {module: 35, video:2}; const obj2= {module: 35, video:2};
+// console.log(obj1 === obj2);
 
-const getGirlFriend= (name = "chokina")=>"name"; console.log(getGirlFriend()); 
+// const getGirlFriend= (name = "chokina")=>"name"; console.log(getGirlFriend());
+
+
