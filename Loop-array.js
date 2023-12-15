ad35e35e9cebdfcdaf72e956b1e44b0b93d4f379
rakibul-islam <raakibul.uap@gmail.com>
2023-12-15 19:36:50 +0600

    sum array

--- Loop-array.js
+++ Loop-array.js
@@ -1,11 +1,24 @@
-var number = [10,20,30,40,50,60,70,80]
+// var number = [10,20,30,40,50,60,70,80]
 
 // console.log(number[0]);
 
-var sum = 0;
-for(i=0;i<number.length;i++){
-    console.log(number[i]);
-    sum = sum + number[i];
+// var sum = 0;
+// for(i=0;i<number.length;i++){
+//     console.log(number[i]);
+//     sum = sum + number[i];
+// }
+
+// console.log("sum = ",sum);
+
+var num = new Array();
+
+for( var i = 0; i<5; i++){
+    num[i] = prompt("Enter a number");
 }
 
-console.log("sum = ",sum);
+sum = 0;
+
+for(i = 0; i<5; i++){
+    sum = sum + num[i];
+}
+console.log("Sum of numbers: " + sum);
