c4ddd24c5fd79a43551595aab61994dadc24845e
rakibul-islam <raakibul.uap@gmail.com>
2023-12-19 22:06:20 +0600

    math object for Javascript

---
+++ Math-object.js
@@ -0,0 +1,7 @@
+sq = Math.sqrt(34)
+console.log(sq);
+console.log(Math.abs(-45));
+console.log(Math.floor(33.45));
+console.log(Math.pow(2,4));
+console.log(Math.sin(3));
+console.log(Math.ceil(3.45));
