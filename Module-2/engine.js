21d99aef58b2cea2ae4d9ec8a6f194cb4bc51a27
rakibul-islam <raakibul.uap@gmail.com>
2025-04-10 17:52:32 +0600

    Module added

--- Module-2/engine.js
+++ Module-2/engine.js
@@ -6,3 +6,15 @@ function jsengine(code){
 
 result = jsengine('var a = 55')
 console.log(result);
+
+// interpreter vs compiler
+function someCalculation(x,y){
+    add = x + y
+    console.log(add);
+}
+
+for(let i =0;i<1000;i++){
+    // 
+}
+someCalculation(5,4)
+
