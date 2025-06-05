ba792ffc06fee60c177161c9a795e50f92e7d0c2
rakibul-islam <raakibul.uap@gmail.com>
2025-06-05 22:48:26 +0600

    ES6 code

--- ES6/dynamic_function.js
+++ ES6/dynamic_function.js
@@ -5,3 +5,24 @@ var f = function(nameValue){
 }
 let print = f("Abdur Rahim")
 console.log(print);
+//var
+var name = "Joe"
+name = "Daniel" //reasign
+console.log(name);
+
+// let
+
+let name1 = "Joe";
+name1 = "Daniel"
+console.log(name1);
+
+// const
+const name2 = "Joe"
+// name2 = "Daniel"
+console.log(name2);
+
+function myFun(){
+    var name3 = "ES6";
+    console.log(name3);
+}
+myFun();
