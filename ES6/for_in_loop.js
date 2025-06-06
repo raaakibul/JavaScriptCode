b237a1ecbcea31ae528c6e8f01a4f38a74d3c709
rakibul-islam <raakibul.uap@gmail.com>
2025-06-06 23:37:58 +0600

    decision making

--- ES6/for_in_loop.js
+++ ES6/for_in_loop.js
@@ -10,3 +10,10 @@ for(let prop in Person){
     // console.log(Person[prop]);
     console.log(prop,":" ,Person[prop]);
 }
+if(Person['shirtColor']=='red'){
+    console.log("Shirt color is Red");
+}else if(Person['shirtColor']=='blue'){
+    console.log("Shirt color is blue");
+}else{
+    console.log("Not found");
+}
