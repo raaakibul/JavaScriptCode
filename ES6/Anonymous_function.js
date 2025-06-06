2ad340542ca7b5b9a398db13d595762ce7c36762
rakibul-islam <raakibul.uap@gmail.com>
2025-06-06 23:50:54 +0600

    function

---
+++ ES6/Anonymous_function.js
@@ -0,0 +1,5 @@
+var f = function(...x){
+    return x;    
+}
+console.log(f("Hello"));
+console.log(f(1,2,5,4));
---
+++ ES6/return_function.js
@@ -0,0 +1,9 @@
+function return_function(number){
+    return number;
+}
+function add(x,y){
+    return x+y;
+}
+console.log(return_function("Number"));
+console.log(return_function([1,4,5]));
+console.log(add(5,50));
