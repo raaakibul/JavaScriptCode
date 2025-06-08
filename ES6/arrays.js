596448db96928610c7a98d07b5ab92beeaff8aa7
rakibul-islam <raakibul.uap@gmail.com>
2025-06-08 21:30:43 +0600

    array

---
+++ ES6/arrays.js
@@ -0,0 +1,16 @@
+// Array
+var arr = [1,2,3,5,[4,5,6],"Abdur Rahim"]
+console.log(arr);
+console.log(arr[4]);
+
+let array = new Array("A","B","C","D");
+console.log(array);
+console.log(array[1]);
+
+for(let item of array){
+    console.log(item);
+}
+
+for(let item of arr){
+    console.log(item);
+}
