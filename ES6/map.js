b41d02fa9734794218f849c5d0426563aa578f1c
rakibul-islam <raakibul.uap@gmail.com>
2025-06-08 21:46:38 +0600

    map and array

--- ES6/map.js
+++ ES6/map.js
@@ -3,8 +3,12 @@ var map = new Map();
 map.set("1", "Bangladesh");
 map.set("2", "USA");
 map.set("3", "Finland");
-map.set("4", " Italy");
+map.set("4", "Italy");
 console.log(map);
+
+// map.delete("1");
+// map.clear();
+// map.has(3)
 
 for(let value of map.values()){
     console.log(value);
@@ -12,4 +16,13 @@ for(let value of map.values()){
 
 for(let key of map.keys()){
     console.log(key);
+}
+// delete(key)
+// get(key)
+// clear()
+// has(value)
+if(map.has("1")){
+    console.log("Yes");
+}else{
+    console.log("No");
 }
