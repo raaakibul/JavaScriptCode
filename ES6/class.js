bb6c36796d2298f94e33ad71f46d152456ef14f6
rakibul-islam <raakibul.uap@gmail.com>
2025-06-11 21:12:46 +0600

    class

---
+++ ES6/class.js
@@ -0,0 +1,16 @@
+class ClassName{
+    functionName(){
+        console.log("Function");
+    }
+    functionName1(){
+        console.log("Function1");
+    }
+    functionName2(){
+        console.log("Function2");
+    }
+}
+
+var obj = new ClassName;
+obj.functionName();
+obj.functionName1();
+obj.functionName2();
--- ES6/set.js
+++ ES6/set.js
@@ -1,6 +1,21 @@
 var set_item = new Set();
+
 set_item.add("Green");
 set_item.add("Red");
 set_item.add("Blue");
 set_item.add("Yellow");
+set_item.add("Black");
+
 console.log(set_item);
+
+var set_item_1 = new Set(['A','B','C','D']);
+console.log(set_item_1);
+// set_item_1.clear();
+set_item_1.delete("C");
+console.log(set_item_1);
+
+if(set_item.has("Tomato")){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
