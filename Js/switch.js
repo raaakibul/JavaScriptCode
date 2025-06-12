ada1ad6b8310b2a311e3b496ca80f23a81a0acf1
rakibul-islam <raakibul.uap@gmail.com>
2025-06-12 14:45:50 +0600

    switch

---
+++ Js/switch.js
@@ -0,0 +1,14 @@
+var marks = 56;
+switch(true){
+    case(marks<=100 && marks>=80):
+    console.log("A+");
+    break;
+
+    case(marks<80 && marks>=70):
+    console.log("A");
+    break;
+
+    default:
+        console.log("Failed");
+
+}
