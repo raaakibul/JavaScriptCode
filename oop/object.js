e9ec6d87d44ac6165463d131b59b22a984cbcd0d
rakibul-islam <raakibul.uap@gmail.com>
2025-06-13 23:16:52 +0600

    javascript oop

---
+++ oop/object.js
@@ -0,0 +1,12 @@
+let person = {
+    first_name : "Abdur ",
+    last_name : "Rahim",
+    age: 34,
+    city: 'Dhaka',
+    isBangladeshi :true,
+    getName:()=>{
+        return `My full name is ${person.first_name} ${person.last_name}`;
+    }
+}
+
+console.log(person);
